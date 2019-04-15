290783e38d591730434cf0f2f4d8e234edc7240d
Nikita Ananjevs <nikitaananjev@gmail.com>
2019-04-15 22:14:14 +0200

    vers.2.cleanup.github

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -10,16 +10,16 @@ class Pages extends Controller
 {
     public function __construct()
     {
-        $this->postModel = $this->model('Post');
+
     }
 
     public function index()
     {
-        $posts = $this->postModel->getPosts();
+
 // PASSING DATA
         $data = [
             'title' => 'Welcome',
-            'posts' => $posts
+
         ];
 
         $this->view('pages/index', $data);
